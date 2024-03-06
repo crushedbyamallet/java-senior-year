417a386bbd67e674e078b12e6e671d0ee7c750c3
mary <126797082+crushedbyamallet@users.noreply.github.com>
2024-03-05 22:05:06 -0300

    Add files via upload
    starting out with serializability

---
+++ calculadoraTCPa/src/main/resources/archetype-resources/src/main/java/Resposta.java
@@ -0,0 +1,30 @@
+public class Resposta implements java.io.Serializable {
+
+    private int status;
+    private float result;
+
+    public Resposta (int status, float result) {
+        this.status = status;
+        this.result = result;
+    }
+
+    public Resposta(){
+
+    }
+
+    public int getStatus(){
+        return status;
+    }
+
+    public float getResult(){
+        return result;
+    }
+
+    public void setStatus(int newStatus){
+        status = newStatus;
+    }
+
+    public void setResult(float newResult){
+        result = newResult;
+    }
+}
